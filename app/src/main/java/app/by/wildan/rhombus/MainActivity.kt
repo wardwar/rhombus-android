ad7bf189209acc0b2f218275936a709fac065e4f
Wildan Angga Rahman <wildan.aga@gmail.com>
2020-04-17 08:46:00 +0700

    initial

---
+++ app/src/main/java/app/by/wildan/rhombus/MainActivity.kt
@@ -0,0 +1,78 @@
+package app.by.wildan.rhombus
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            val star = textInputCount.text.toString().toInt()
+            textResult.text = calculate(star)
+        }
+    }
+
+    private fun calculate(star: Int): String {
+        return when {
+            star < 5 -> {
+                Toast.makeText(this, "Min star is 5", Toast.LENGTH_LONG).show()
+                ""
+            }
+            star > 100 -> {
+                Toast.makeText(this, "Max star is 100", Toast.LENGTH_LONG).show()
+                ""
+            }
+            else -> {
+                val stringBuilder = StringBuilder()
+                var space = 0
+                for (i in 1..star / 2) {
+                    if (i == 1) {
+                        if (star % 2 == 1) {
+                            stringBuilder.append("*")
+                        } else {
+                            stringBuilder.append("**")
+                        }
+                    }else{
+                        stringBuilder.append("*")
+                    }
+                    if (space > 0) {
+                        stringBuilder.append(" ".repeat(space))
+                        stringBuilder.append("*")
+                    }
+                    stringBuilder.append(System.getProperty("line.separator"))
+                    space++
+                }
+
+                if (star % 2 == 0) {
+                    space--
+                }
+
+                for (i in star/2 until star) {
+                    if (i == star-1) {
+                        if (star % 2 == 1) {
+                            stringBuilder.append("*")
+                        } else {
+                            stringBuilder.append("**")
+                        }
+                    }else{
+                        stringBuilder.append("*")
+                    }
+
+                    if (space > 0) {
+                        stringBuilder.append(" ".repeat(space))
+                        stringBuilder.append("*")
+                    }
+                    stringBuilder.append(System.getProperty("line.separator"))
+                    space--
+                }
+                stringBuilder.toString()
+            }
+        }
+
+    }
+}
